1793b08d449356d1d8adf1ddb88fbcfbf43b02d1
hello262 <74759062+hello262@users.noreply.github.com>
2020-12-06 19:01:20 +0700

    Update key_test.py

--- key_test.py
+++ key_test.py
@@ -1,6 +1,1 @@
-import hashlib
-key_sha1 = hashlib.sha1(key).hexdigest()
-if key_sha1 == "e2da39989f2ad9dae985c98656b73b93974d62a6" or key_sha1 == "71b9c397444943baee92fc16cc2a7981af676dd6" or key_sha1 == "fad8d029a79f38e525b9b2d28cb6adccac9845d5":
-    status_verify = True
-else:
-    status_verify = False
+keys_sha1 == ["e2da39989f2ad9dae985c98656b73b93974d62a6", "71b9c397444943baee92fc16cc2a7981af676dd6", "fad8d029a79f38e525b9b2d28cb6adccac9845d5"]
